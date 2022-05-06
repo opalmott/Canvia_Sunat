c4741d5378e043053eb08f96e97ae958fb2bc2ba
Oscar Palomino <opalmott@gmail.com>
2022-05-06 12:36:41 -0500

    agregado por no pasar

---
+++ 981-Pases/PAS20221U220100020-SFSW/20220513/script/opcionesMenuIntranet_PAS20221U220100019.sql
@@ -0,0 +1,10 @@
+﻿--- 20211001 CREAR OPCION DE MENU: REQUERIMIENTO DE DOCUMENTOS CONSERVADOS
+--- Conectarse al Informix, ONCONFIG=onconfig.s0_menu, Base de Datos = BDIASGME con usuario DBA o personalizado y ejecutar los siguientes scripts
+
+insert into t1508grupo (cod_pestana, cod_agrupacion, cod_sistema, cod_grupo, des_nombre, cod_padre, num_orden, ind_del, cod_usumodif, fec_modif) 
+values ('22', '1', '1', '6', 'Consultas', '0', '6', '1', user, CURRENT);
+
+insert into t1506programa (cod_pestana, cod_agrupacion, cod_sistema, cod_grupo, cod_programa, des_nombre, des_contexto, des_servlet, fec_inivig, fec_finvig, hor_inicio, hor_fin, num_orden_pestana, num_orden_agrupa, num_orden_sistema, num_orden_grupo, num_orden_programa, ind_acceso, tip_invoca, ind_del, cod_usumodif, fec_modif, des_subdominio) 
+values ('22', '1', '1', '6', '1', 'Declaración por Tipo', '/app/controladuaneroms/ingresosalida/sicpa/consultas', '/consulta-declaraciones', CURRENT, TO_DATE ('12-31-2050', '%m-%d-%Y'), TO_DATE ('00:00', '%H:%M'), TO_DATE ('23:59', '%H:%M'), '22', '1', '1', '6', '1', '1', '4', '1', user, CURRENT, 'intranet');
+
+commit;
---
+++ 981-Pases/PAS20221U220100020-SFSW/20220513/script/opcionesMenuIntranet_PAS20221U220100019_reversion.sql
@@ -0,0 +1,5 @@
+﻿--- 20211001 CREAR OPCION DE MENU: REQUERIMIENTO DE DOCUMENTOS CONSERVADOS
+--- Conectarse al Informix, ONCONFIG=onconfig.s0_menu, Base de Datos = BDIASGME con usuario DBA o personalizado y ejecutar los siguientes scripts
+delete from t1506programa where cod_pestana ='22' and cod_agrupacion = '1' and cod_sistema = '1' and cod_grupo = '6' and cod_programa = '1';
+delete from t1508grupo where cod_pestana ='22' and cod_agrupacion = '1' and cod_sistema = '1' and cod_grupo = '6';
+commit;
